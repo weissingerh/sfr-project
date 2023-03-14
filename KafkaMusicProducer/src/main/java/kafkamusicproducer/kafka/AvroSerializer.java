e00a3e89a963f3c56092b39408487da99e094fea
Hanna Weissinger <hanna@Hannas-MacBook-Pro.local>
2023-03-14 18:00:31 +0100

    Add Avro Serializer function

---
+++ KafkaMusicProducer/src/main/java/kafkamusicproducer/kafka/AvroSerializer.java
@@ -0,0 +1,37 @@
+package kafkamusicproducer.kafka;
+
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.*;
+
+import java.io.*;
+
+
+public class AvroSerializer {
+    static byte[] fromJsonToAvro(String json, String avroSchema) throws Exception {
+        InputStream input = new ByteArrayInputStream(json.getBytes());
+        DataInputStream din = new DataInputStream(input);
+
+        Schema.Parser schemaParser = new Schema.Parser();
+        Schema schema = schemaParser.parse(avroSchema);
+
+        Decoder decoder = DecoderFactory.get().jsonDecoder(schema, din);
+
+        DatumReader<Object> reader = new GenericDatumReader<>(schema);
+        Object datum = reader.read(null, decoder);
+
+        GenericDatumWriter<Object> w = new GenericDatumWriter<>(schema);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        Encoder e = EncoderFactory.get().binaryEncoder(outputStream, null);
+
+        w.write(datum, e);
+        e.flush();
+
+        return outputStream.toByteArray();
+    }
+}
